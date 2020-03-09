81571cf0bf91e263a8e23d96bae63c6cc576c7c0
Orta <orta.therox+github@gmail.com>
2020-03-09 18:51:31 -0400

    tag targets

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
+namespace SpriteKind {
+    export const TaggingSpot = SpriteKind.create()
+}
 namespace myTiles {
     //% blockIdentity=images._tile
     export const tile0 = img`
@@ -56,6 +59,25 @@ d d d d d d d d d d d d d d d d
 d d d d d d d d d d d d d d d d 
 d d d d d d d d d d d d d d d d 
 `
+    //% blockIdentity=images._tile
+    export const tile4 = img`
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+a a a a a a a a a a a a a a a a 
+a a a a a a a a a a a a a a a a 
+a a a a a a a a a a a a a a a a 
+a a a a a a a a a a a a a a a a 
+a a a a a a a a a a a a a a a a 
+a a a a a a a a a a a a a a a a 
+a a a a a a a a a a a a a a a a 
+a a a a a a a a a a a a a a a a 
+a a a a a a a a a a a a a a a a 
+a a a a a a a a a a a a a a a a 
+a a a a a a a a a a a a a a a a 
+a a a a a a a a a a a a a a a a 
+a a a a a a a a a a a a a a a a 
+a a a a a a a a a a a a a a a a 
+a a a a a a a a a a a a a a a a 
+`
 }
 controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
     tagger.setImage(img`
@@ -97,6 +119,9 @@ controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
 . . . . . . . . . . . . . . . . 
 `)
 })
+function moveTaggingSpot () {
+    tiles.placeOnRandomTile(tagTarget, myTiles.tile4)
+}
 controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
     tagger.setImage(img`
 . . . . . . . . . . . . . . . . 
@@ -137,9 +162,10 @@ controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
 . . . . . . . . . . . . . . . . 
 `)
 })
+let tagTarget: Sprite = null
 let tagger: Sprite = null
 tiles.setTilemap(tiles.createTilemap(
-            hex`1c00140002020202020202020202020202020202040808080e0303030303030302111111111111111111111102020202040808080e0303030303030302111111111111111111111102020202040808080e0303030303030302020202020202020202020202020202040808080e0303030303030302020202020202020202020202020202070808080e0303030303030302111111111111020202111111111111040808080e0303030303030302111111111111020202111111111111040808080e0303030303030302020202020202020202020202020202040808080e0303030303030302020202020202020202020202020202040808080e0303030303030311111111111111111111111102020202040808080e0303030303030311111111111111111111111102020202070808080e0303030303030302020202020202020202020202020202040808080e0303030303030302020202020202020202020202020202040808080e0303030303030302111111111111110211111111111102040808080e0303030303030302111111111111110211111111111102040808080e0303030303030302020202020202020202020202020202040808080e0303030303030302020202020202020202020202020202070808080e0303030303030302020202111111111111111102020202040808080e0303030303030302020202111111111111111102020202040808080e0303030303030302020202020202020202020202020202040808080e03030303030303`,
+            hex`1c00140002020202020202020202020202020202040808080e0303030303030302111111111111111111111102020202040808080e0303030303030302111111111111111111111102020202040808080e0303030303030302121212121212121212121202020202040808080e0303030303030302020202020202020202020202020202070808080e0303030303030302111111111111020202111111111111040808080e0303030303030302111111111111020202111111111111040808080e0303030303030302121212121212020202121212121212040808080e0303030303030302020202020202020202020202020202040808080e0303030303030311111111111111111111111102020202040808080e0303030303030311111111111111111111111102020202070808080e0303030303030312121212121212121212121202020202040808080e0303030303030302020202020202020202020202020202040808080e0303030303030302111111111111110211111111111102040808080e0303030303030302111111111111110211111111111102040808080e0303030303030302121212121212120212121212121202040808080e0303030303030302020202020202020202020202020202070808080e0303030303030302020202111111111111111102020202040808080e0303030303030302020202111111111111111102020202040808080e0303030303030302020202121212121212121202020202040808080e03030303030303`,
             img`
 . . . . . . . . . . . . . . . . 2 . . . . . . . . . . . 
 . 2 2 2 2 2 2 2 2 2 2 2 . . . . 2 . . . . . . . . . . . 
@@ -162,7 +188,7 @@ tiles.setTilemap(tiles.createTilemap(
 . . . . 2 2 2 2 2 2 2 2 . . . . 2 . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 2 . . . . . . . . . . . 
 `,
-            [myTiles.tile0,sprites.builtin.forestTiles23,myTiles.tile1,sprites.castle.tilePath5,sprites.builtin.brick,sprites.vehicle.roadVertical,sprites.vehicle.roadHorizontal,sprites.dungeon.doorOpenNorth,sprites.castle.tileGrass1,sprites.castle.tilePath1,sprites.castle.tileGrass2,sprites.castle.tileGrass3,sprites.castle.tilePath2,sprites.castle.tilePath7,sprites.castle.tilePath4,sprites.castle.tilePath8,sprites.builtin.forestTiles24,myTiles.tile3],
+            [myTiles.tile0,sprites.builtin.forestTiles23,myTiles.tile1,sprites.castle.tilePath5,sprites.builtin.brick,sprites.vehicle.roadVertical,sprites.vehicle.roadHorizontal,sprites.dungeon.doorOpenNorth,sprites.castle.tileGrass1,sprites.castle.tilePath1,sprites.castle.tileGrass2,sprites.castle.tileGrass3,sprites.castle.tilePath2,sprites.castle.tilePath7,sprites.castle.tilePath4,sprites.castle.tilePath8,sprites.builtin.forestTiles24,myTiles.tile3,myTiles.tile4],
             TileScale.Sixteen
         ))
 tagger = sprites.create(img`
@@ -185,3 +211,17 @@ tagger = sprites.create(img`
 `, SpriteKind.Player)
 scene.cameraFollowSprite(tagger)
 controller.moveSprite(tagger)
+tagTarget = sprites.create(img`
+. . 7 7 7 7 7 7 7 7 7 7 7 7 . . . . . . . . . . . . 
+. 7 . . . . . . . . . . . . 7 . 7 . . . . . 7 7 7 7 
+. . . . . 7 7 . . . . 7 7 7 7 7 7 . . . . 7 . 7 . . 
+. . . . . . . . . . 7 . . . 7 7 7 7 7 7 7 7 7 7 7 7 
+. . . . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . . . 
+`, SpriteKind.TaggingSpot)
+moveTaggingSpot()
